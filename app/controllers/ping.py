7eb431358af7e40910edfdd847fb43743b0c2f51
wangjq4214 <w1637894214@163.com>
2019-10-15 00:31:34 +0800

    添加数据库操作范例

--- app/controllers/ping.py
+++ app/controllers/ping.py
@@ -1,11 +1,20 @@
 from aiohttp import web
-
+from app.models.notice import Notice
 
 ping = web.RouteTableDef()
 
 
 @ping.get('')
 async def hello(request: web.BaseRequest):
+    notice = Notice()
+    res = await notice.select_all()
+    data = []
+    for item in res:
+        temp = dict(item)
+        temp['push_day'] = temp['push_day'].strftime('%Y-%m-%d')
+        data.append(temp)
+
     return web.json_response({
-        'status': 'success'
+        'status': 'success',
+        'data': data
     })
--- app/models/notice.py
+++ app/models/notice.py
@@ -1,19 +1,20 @@
 import sqlalchemy as sa
-from aiomysql.sa import connection
+from . import engine
 
 metadata = sa.MetaData()
-conn = connection()
-
-notice = sa.Table(
-    'notice', metadata,
-    sa.Column('id', sa.INT, primary_key=True, autoincrement=True),
-    sa.Column('title', sa.VARCHAR(255), nullable=False),
-    sa.Column('summary', sa.VARCHAR(1024), nullable=False),
-    sa.Column('content', sa.VARCHAR(1024), nullable=True),
-    sa.Column('push_day', sa.DATETIME, nullable=False)
-)
 
 
-async def return_notice():
-    sql = notice.select()
+class Notice:
+    notice = sa.Table(
+        'notice', metadata,
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('title', sa.VARCHAR(255), nullable=False),
+        sa.Column('summary', sa.VARCHAR(1024), nullable=False),
+        sa.Column('content', sa.VARCHAR(1024), nullable=True),
+        sa.Column('push_day', sa.DateTime, nullable=False)
+    )
 
+    async def select_all(self):
+        async with engine.engine.acquire() as conn:
+            res = await conn.execute(self.notice.select())
+            return await res.fetchall()
