ccb39f172fbdf1164d531063ca7c2eb14a520bd0
wangjq4214 <1637894214@qq.com>
2019-10-30 23:29:00 +0800

    完成统计接口

--- app/controllers/record.py
+++ app/controllers/record.py
@@ -2,11 +2,21 @@
 __author__ = 'Abbott'
 
 from flask import Blueprint, g
-from app.utils.warpper import success_warp
+from sqlalchemy.exc import SQLAlchemyError
+from app.utils.warpper import success_warp, fail_warp
+from app.models.record import get_record
 
 record_page = Blueprint('record', __name__, url_prefix='/record')
 
 
 @record_page.route('', methods=['GET'])
 def get_count():
-    return success_warp(g.username)
+    username = g.username
+    try:
+        today, week_record = get_record(username)
+        return success_warp({
+            'day': today,
+            'week': int(week_record)
+        })
+    except SQLAlchemyError:
+        return fail_warp('数据库操作失败')
--- app/models/record.py
+++ app/models/record.py
@@ -1,2 +1,22 @@
 # -*-coding:utf8-*-
 __author__ = 'Abbott'
+
+import datetime
+from datetime import timedelta
+from sqlalchemy import and_
+from sqlalchemy.sql import func
+from app.models import db
+from app.models.models import User, Record
+
+
+def get_record(username: str):
+    now = datetime.date.today()
+    this_week_start = now - timedelta(days=now.weekday())
+    this_week_end = now + timedelta(days=6 - now.weekday())
+
+    user = User.query.filter_by(username=username).first()
+    today_record = Record.query.filter_by(user_id=user.id).filter_by(day=now).first()
+    week_record = db.session.query(func.sum(Record.count).label('sums')).select_from(Record). \
+        filter(and_(Record.day >= this_week_start, Record.day <= this_week_end)).first()
+
+    return today_record.count, week_record.sums
