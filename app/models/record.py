9521aa3d54935a5e8a0798cf454f506ba1b3cb57
wangjq4214 <1637894214@qq.com>
2019-10-31 00:12:21 +0800

    修复统计

--- app/models/record.py
+++ app/models/record.py
@@ -16,7 +16,11 @@ def get_record(username: str):
 
     user = User.query.filter_by(username=username).first()
     today_record = Record.query.filter_by(user_id=user.id).filter_by(day=now).first()
+    if today_record is None:
+        temp = Record(day=now, count=0, user_id=user.id)
+        db.session.add(temp)
+        today_record = Record.query.filter_by(user_id=user.id).filter_by(day=now).first()
     week_record = db.session.query(func.sum(Record.count).label('sums')).select_from(Record). \
-        filter(and_(Record.day >= this_week_start, Record.day <= this_week_end)).first()
+        filter(and_(Record.day >= this_week_start, Record.day <= this_week_end, Record.user_id == user.id)).first()
 
     return today_record.count, week_record.sums
