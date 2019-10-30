c9d98c4aa068643eae4744622021fa2e12f44062
wangjq4214 <1637894214@qq.com>
2019-10-30 21:36:31 +0800

    修正today

--- app/models/tag.py
+++ app/models/tag.py
@@ -20,9 +20,9 @@ def tag_it(img_id: int, tag: list, username: str):
 
     # 用户记录加一
     user = User.query.filter_by(username=username).first()
-    record = Record.query.filter_by(user_id=user.id).filter_by(day=datetime.datetime.now()).first()
+    record = Record.query.filter_by(user_id=user.id).filter_by(day=datetime.date.today()).first()
     if record is None:
-        temp = Record(day=datetime.datetime.now(), count=1, user_id=user.id)
+        temp = Record(day=datetime.date.today(), count=1, user_id=user.id)
         db.session.add(temp)
     else:
         record.count = record.count + 1
