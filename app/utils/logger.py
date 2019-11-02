ee05d4fb8ce4ee7b9bff4655de2696abcd7a0276
wangjq4214 <w1637894214@163.com>
2019-11-02 20:51:48 +0800

    添加log路径

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -2,14 +2,18 @@
 __author__ = 'Abbott'
 
 import logging
+import pathlib
 from logging.handlers import TimedRotatingFileHandler
 from flask import Flask
 
 
 def create_base_log(app: Flask):
+    path = pathlib.Path(__file__).parent.parent.parent
+    path = pathlib.Path.joinpath(path, 'log', 'flask.log')
+
     handler = TimedRotatingFileHandler(
-        "flask.log", when="D", interval=1, backupCount=15,
-        encoding="UTF-8", delay=False, utc=False)
+        path, when='D', interval=1, backupCount=15,
+        encoding='UTF-8', delay=False, utc=False)
 
     formatter = logging.Formatter('[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s][%(thread)d] - %(message)s')
     handler.setFormatter(formatter)
